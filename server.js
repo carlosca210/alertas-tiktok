1c53c3b5e421c708277cfc18b231987282bbe37b
CarlosCabrita <ccabrita210@gmail.com>
2026-05-08 14:25:48 -0400

    configurado de regalos y console log 2.0

--- server.js
+++ server.js
@@ -42,16 +42,16 @@ io.on('connection', (socket) => {
             console.log(`🎁 REGALO DETECTADO -> Nombre: ${data.giftName} | ID: ${data.giftId} | Cantidad: ${data.repeatCount}`);//este log aparece si alguien manda regalo 
             io.emit('show-alert', { gift: data.giftName });
         });
-    });
 
-    // 2. CAPTURAR CHAT (Solo para probar si la conexión está viva)
-    tiktokConn.on('chat', (data) => {
-        console.log(`💬 Chat de ${data.uniqueId}: ${data.comment}`);
-    });
+        // 2. CAPTURAR CHAT (Solo para probar si la conexión está viva)
+        tiktokConn.on('chat', (data) => {
+            console.log(`💬 Chat de ${data.uniqueId}: ${data.comment}`);
+        });
 
-    // 3. CAPTURAR ERRORES DE LA LIBRERÍA
-    tiktokConn.on('error', (err) => {
-        console.error("⚠️ Error en el stream de datos:", err);
+        // 3. CAPTURAR ERRORES DE LA LIBRERÍA
+        tiktokConn.on('error', (err) => {
+            console.error("⚠️ Error en el stream de datos:", err);
+        });
     });
 
     // EVENTO 2: Prueba manual desde el botón del Panel
